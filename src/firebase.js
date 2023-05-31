0933edd993cbfdac54dc5817061ee8265631a9a6
vsbecinstitution <104641987+vsbecinstitution@users.noreply.github.com>
2023-05-31 20:35:32 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,26 +1,27 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import {getDatabase} from "firebase/database"
 import { getAuth } from "firebase/auth";
-
+import { getAnalytics } from "firebase/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyC-T_aOKHU74kFg8EZ19YfYok3ChFCP4_k",
-  authDomain: "video-logger-66b46.firebaseapp.com",
-  projectId: "video-logger-66b46",
-  storageBucket: "video-logger-66b46.appspot.com",
-  messagingSenderId: "1028298676520",
-  appId: "1:1028298676520:web:8b3e240c800e834e1c981d",
-  measurementId: "G-V5K5SD8DK9",
+  apiKey: "AIzaSyAQ2q4Xzbo_DjEJWxZgLBC9bnwR5Jxup7U",
+  authDomain: "vsbec-communication-video.firebaseapp.com",
+  databaseURL: "https://vsbec-communication-video-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "vsbec-communication-video",
+  storageBucket: "vsbec-communication-video.appspot.com",
+  messagingSenderId: "822584761631",
+  appId: "1:822584761631:web:41990e9bfba910c195642a",
+  measurementId: "G-PM0K9G1JPV"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const rtdb = getDatabase(app);
 export const auth = getAuth(app);
